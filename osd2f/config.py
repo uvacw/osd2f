25ddc0acb5f3aaf528d1390fd51f27dc3f045838
bobvdvelde <rnvandevelde@gmail.com>
2021-02-04 00:08:04 +0100

    added DB URL for configs

--- osd2f/config.py
+++ osd2f/config.py
@@ -10,10 +10,12 @@ class Config:
 
 class Testing(Config):
     TESTING = True
+    DB_URL = "sqlite://"
 
 
 class Development(Config):
     DEBUG = True
+    DB_URL = _os.environ.get("OSD2F_DB_URL", "sqlite:memory")
 
 
 class Production(Config):
@@ -21,6 +23,7 @@ class Production(Config):
     TESTING = False
     BIND = "0.0.0.0"
     SECRET_KEY = _os.environ.get("OSD2F_SECRET")
+    DB_URL = _os.environ.get("OSD2F_DB_URL")
 
 
 # hypercorn
