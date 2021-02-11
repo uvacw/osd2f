7ca1bf6ac4a0298d8956f8c7bfec9523cc5e6422
bobvdvelde <rnvandevelde@gmail.com>
2021-02-11 02:07:39 +0100

    minor clarification

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,13 @@ module.exports = {
     libraryTarget: 'window',
     path: path.resolve(__dirname, 'osd2f', 'static','js'),
   },
+
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
         {
-          
+          // libarchive requires the distribution bundles to be available
+          // for the web worker.          
           from: path.resolve(__dirname, "node_modules","libarchive.js","dist"), 
           to: path.resolve(__dirname, "osd2f","static","js","libarchive")
         }
