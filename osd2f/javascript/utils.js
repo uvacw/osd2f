e291d0ca15fd6426e0f7a923124591c988e9892d
bobvdvelde <rnvandevelde@gmail.com>
2021-02-15 23:19:39 +0100

    formatting

--- osd2f/javascript/utils.js
+++ osd2f/javascript/utils.js
@@ -1,28 +1,27 @@
-"use strict"
+'use strict'
 // as per https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-export function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
+export function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 // Data could be too big for sessionStorage (>10mb)
 // so instead, we park it into a private variable
-// accessible via the Donation class 
-var donation = new Array
+// accessible via the Donation class
+var donation = new Array()
 export class Donation {
-
-  SetData(data){
+  SetData (data) {
     donation.push(...data)
   }
 
-  ClearData(){
-    donation = new Array
+  ClearData () {
+    donation = new Array()
   }
 
-  GetRaw(){
+  GetRaw () {
     console.log(donation)
     return JSON.stringify(donation)
   }
-  GetObj(){
+  GetObj () {
     return null
   }
 }
