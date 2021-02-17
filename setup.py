4f4123315a42a61bd4ebd07215844454faa3a46f
bobvdvelde <rnvandevelde@gmail.com>
2021-02-17 20:39:32 +0100

    version in major.minor.patch format

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="OSD2F",
     python_requires=">=3.9",
-    version="0.0-pre-release-alpha",
+    version="0.0.0",
     description="Open Source Data Donation Framework",
     author="Bob van de Velde",
     author_email="osd2f@bob-as-a-service.com",
