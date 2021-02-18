8a52c62f524bac1207d60f85f670e7adf62a098f
bobvdvelde <rnvandevelde@gmail.com>
2021-02-18 02:29:25 +0100

    support for Vue build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,53 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname,"osd2f", "javascript", "file_upload.js"),
+  module: {
+    rules: [
+      {
+        test: /.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.vue$/,
+        use: ['vue-loader']
+      }
+    ]
+  },
+
+  entry: path.resolve(__dirname, 'osd2f', 'javascript', 'file_upload.js'),
+
   output: {
     filename: 'main.js',
     library: 'file_upload',
     libraryTarget: 'window',
-    path: path.resolve(__dirname, 'osd2f', 'static','js'),
+    path: path.resolve(__dirname, 'osd2f', 'static', 'js')
+  },
+
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
   },
 
   plugins: [
+    new VueLoaderPlugin(),
     new CopyWebpackPlugin({
       patterns: [
         {
           // libarchive requires the distribution bundles to be available
-          // for the web worker.          
-          from: path.resolve(__dirname, "node_modules","libarchive.js","dist"), 
-          to: path.resolve(__dirname, "osd2f","static","js","libarchive")
+          // for the web worker.
+          from: path.resolve(
+            __dirname,
+            'node_modules',
+            'libarchive.js',
+            'dist'
+          ),
+          to: path.resolve(__dirname, 'osd2f', 'static', 'js', 'libarchive')
         }
       ]
     })
   ]
-};
+}
