67a2221a9363c5a653f851bba0f42a9163815fdf
bobvdvelde <rnvandevelde@gmail.com>
2021-03-05 11:25:24 +0100

    corrected code version; pinned quart because of upstream bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="OSD2F",
     python_requires=">=3.9",
-    version="0.0-pre-release-alpha",
+    version="0.0.1",
     description="Open Source Data Donation Framework",
     author="Bob van de Velde",
     author_email="osd2f@bob-as-a-service.com",
@@ -14,5 +14,7 @@ setup(
     packages=find_packages(),
     package_data={"osd2f": ["static/*", "templates/*", "settings/*"]},
     scripts=["bin/osd2f"],
-    install_requires=["quart", "pyyaml", "pydantic", "tortoise-orm", "asyncpg"],
+    # quart is version pinned due to a bug in the 0.14.X versions
+    # pending resolution of: https://gitlab.com/pgjones/quart/-/issues/398
+    install_requires=["quart<0.14.0", "pyyaml", "pydantic", "tortoise-orm", "asyncpg"],
 )
