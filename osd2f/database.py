ed94e9768acd189b7af8660a0c197c3f356d5a6c
bobvdvelde <rnvandevelde@gmail.com>
2021-04-21 23:39:07 +0200

    added a database logging handler and worker

--- osd2f/database.py
+++ osd2f/database.py
@@ -1,3 +1,6 @@
+import asyncio
+import logging.handlers
+import queue
 import typing
 
 from tortoise import Tortoise, fields
@@ -25,7 +28,7 @@ class DBLog(Model):
     insert_timestamp = fields.DatetimeField(auto_now_add=True)
     log_level = fields.CharField(index=True, max_length=100, null=False)
     log_source = fields.CharField(index=True, max_length=100, null=False)
-    log_position = fields.CharField(index=True, max_length=100, null=False)
+    log_position = fields.CharField(index=True, max_length=5000, null=False)
     log_sid = fields.CharField(index=True, max_length=100, null=True)
     user_agent_string = fields.CharField(max_length=5000, null=True)
     log_entry = fields.JSONField(null=True)
@@ -100,3 +103,35 @@ async def insert_submission_list(submissionlist: SubmissionList):
 
 async def count_submissions():
     return await DBSubmission.all().count()
+
+
+def add_database_logging() -> queue.SimpleQueue:
+    """Forward logger statements to the database.
+
+    Uses a QueueHandler and an asyncronous worker to
+    insert logs from logger.debug/info/warning/critical
+    to the application database.
+
+    NOTE: messages over 5000 characters are shortened
+    """
+
+    async def async_log_worker(q: queue.SimpleQueue):
+        while 1:
+            try:
+                m = q.get_nowait()
+                if m.msg == "stop-logging":
+                    break
+                if len(m.msg) < 5000:
+                    await insert_log("server", m.levelname, m.msg)
+                else:
+                    await insert_log("server", m.levelname, m.msg[:4997] + "...")
+            except queue.Empty:
+                await asyncio.sleep(0.1)
+
+    logQueue = queue.SimpleQueue()
+    h = logging.handlers.QueueHandler(logQueue)
+    h.setLevel(logger.level)
+    print(h.level)
+    logger.addHandler(h)
+    asyncio.get_running_loop().create_task(async_log_worker(logQueue))
+    return logQueue
