a3ba1802b1aaacb8b9f642b0ef566d6e06f35a8c
bob van de Velde <rnvandevelde@gmail.com>
2021-10-01 08:23:39 +0200

    formatting

--- osd2f/security/entry_encryption.py
+++ osd2f/security/entry_encryption.py
@@ -1,10 +1,9 @@
 import base64
-import os
 import json
+import os
 from typing import Any, Dict
 
 from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
