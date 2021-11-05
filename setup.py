d3cc164d623fd389d77063deb099bab309f6581f
bobvandevelde <rnvandevelde@gmail.com>
2021-11-05 15:27:02 +0100

    typo in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Open Source Data Donation Framework",
     author="Bob van de Velde",
     author_email="osd2f@bob-as-a-service.com",
-    license=open("LICENCE").read(),
+    license=open("LICENSE").read(),
     url="https://github.com/uvacw/osd2f",
     packages=find_packages(),
     package_data={
